29f921828e9dc81d2ac7fd560f5acbbf16359dfd
vitor <vecarrilho@unesc.net>
2024-06-24 20:08:21 -0300

    Adição de contador de sentenças e documentação

--- actions.py
+++ actions.py
@@ -1,24 +1,15 @@
-"""
-A bare-bone AI Action template
-
-Please check out the base guidance on AI Actions in our main repository readme:
-https://github.com/sema4ai/actions/blob/master/README.md
-
-"""
-
 from sema4ai.actions import action
 
 
 @action
-def greet(search_sentence: str) -> str:
+def news_scrapper(search_sentence: str, topic: str) -> str:
     """
-    An empty AI Action Template.
+    Search LA Times news.
 
     Args:
         search_sentence (str): The sentence that will be search in LA Times
+        topic (str): The topic to filter the search in LA Times [World & Nation, Opinion, Politics, Sports, Business]
 
-    Returns:
-        Simple "Hello world" message.
     """
 
     import time
@@ -34,70 +25,55 @@ def greet(search_sentence: str) -> str:
     # options.add_argument("--headless=new")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("start-maximized") 
-
     driver = webdriver.Chrome(options=options)
 
-    # url = "https://www.aljazeera.com/search"
     url = "https://www.latimes.com/search"
-    # new_url = url + 'trump'
-    # new_url = new_url + '?sort=date'
-    # new_url = new_url + '?sort=relevance'
 
     list = []
 
-    class News:
-        def __init__(self, title, description, date, filename, count_search_phrase, has_money):
-            self.title = title
-            self.description = description
-            self.date = date
-            self.filename = filename
-            self.count_search_phrase = count_search_phrase
-            self.has_money = has_money
-
     driver.get(url)
 
-    # wait for page to load
-    # element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//input[@class='search-results-module-input']")))
-
-    # search_value = input('Search Phrase: ')
+    # Search the inputs in the driver
     search_input = driver.find_element(By.XPATH, "//input[@class='search-results-module-input']")
     search_input.click()
     search_input.send_keys(search_sentence)
+    search_topic = driver.find_element(By.XPATH, "//label[@class='checkbox-input-label']//span[text()='"+topic+"']")
+    search_topic.click()
     button_submit = driver.find_element(By.XPATH, "//button[@class='button submit-button']")
     button_submit.click()
 
     time.sleep(5)
 
-    # element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//ul[@class='search-results-module-results-menu']//li")))
+    # Find all the NEWS
     lis_raw = driver.find_elements(By.XPATH, "//ul[@class='search-results-module-results-menu']//li")
     count = len(lis_raw)
 
     # Populate the object with the variables
     for i in range(0, count):
+        count_phrases = 0
+
+        # Save the variables of each NEWS
         title = lis_raw[i].find_element(By.XPATH, ".//h3[@class='promo-title']//a[@class='link']")
         description = lis_raw[i].find_element(By.XPATH, ".//div[@class='promo-content']//p[@class='promo-description']")
         date = lis_raw[i].find_element(By.XPATH, ".//div[@class='promo-content']//p[string-length(@data-timestamp)>0]").get_attribute("data-timestamp")
         try:
             image = lis_raw[i].find_element(By.XPATH, ".//picture//img[@class='image']").get_attribute("alt")
         except NoSuchElementException:
-            image = ''
+            image = 'This file does not contain a filename'
 
-        list.append((title.text, description.text, date, image, 2, True))
+        try:
+            image_src = lis_raw[i].find_element(By.XPATH, ".//picture//img[@class='image']").get_attribute("src")
+        except NoSuchElementException:
+            image_src = 'This file does not contain a SRC'
+
+        count_phrases = title.text.count(search_sentence)
+        count_phrases += description.text.count(search_sentence)
+        list.append((title.text, description.text, date, image, count_phrases, False, image_src))
         
         continue
 
-    df = pd.DataFrame(list, columns=['title', 'description', 'date', 'image_filename', 'count_phrases', 'has_money'])
-    # df.to_excel('planilha.xlsx')
-    return df.to_excel('planilha.xlsx')
-    # Write the informations into the worksheet
-    # workbook = xlsxwriter.Workbook('planilha.xlsx')
-    # worksheet = workbook.add_worksheet("Minha Planilha")
+    # Create a Excel File with the informations
+    df = pd.DataFrame(list, columns=['title', 'description', 'date', 'image_filename', 'count_phrases', 'has_money', 'image_src'])
+    df.to_excel('planilha.xlsx')
 
-    # worksheet.write('A1', 'Nome')
-    # worksheet.write('B1', 'Idade')
-    # worksheet.write('A2', 'João')
-    # worksheet.write('B2', 25)
-
-    # workbook.close()
-
-    # time.sleep(10)
+    return "Sentence searched"
